ced25332a2af4d598a91ddcf14694bcc33c31a02
PHJ <legowww@gmail.com>
2024-05-07 15:03:16 +0900

    feat: 불필요한 의존성 제거

--- modules/api/src/test/java/com/bigs/api/ApiApplicationTests.java
+++ modules/api/src/test/java/com/bigs/api/ApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.bigs.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class ApiApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
